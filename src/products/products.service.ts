57086f3de951b2f03d93fe2f0a958c68014af866
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-02-27 16:53:25 -0500

    feat: funcion connect prisma/client

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class ProductsService {
+export class ProductsService extends PrismaClient implements OnModuleInit {
+
+  private readonly logger = new Logger('ProductsService');
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Connected to the database');
+  }
+
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
