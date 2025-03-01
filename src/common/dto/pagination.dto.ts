7b105b54e0496016cc85ce7655a2b39616f513d7
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-03-01 15:37:16 -0500

    fix: optional attributes dto

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -5,10 +5,10 @@ export class PaginationDto {
     @IsPositive()
     @IsOptional()
     @Type(() => Number)
-    page?: number = 1;
+    page: number = 1;
 
     @IsPositive()
     @IsOptional()
     @Type(() => Number)
-    limit?: number = 10;
+    limit: number = 10;
 }
